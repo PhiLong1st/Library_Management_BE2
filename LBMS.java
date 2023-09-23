aec10a901f2d69ca1a55fbf8b7196036ed5e2f85
Vietphong8520 <121706209+Vietphong8520@users.noreply.github.com>
2023-09-24 06:33:49 +0700

    Add files via upload

---
+++ LBMS.java
@@ -0,0 +1,53 @@
+
+// import model.inforInput;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import view.*;
+
+public class LBMS {
+    public static void main(String[] args) {
+        
+          
+          Menu menuView = new Menu();
+          if (menuView.Login()) {// staff
+          menuView.StaffHomePage();
+          } else {// user
+          menuView.UserHomePage();
+          }
+         
+        String url = "jdbc:mysql://localhost:3306/Library_Management_System";
+        String username = "Phong";
+        String password = "Shii8520";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM Users WHERE UserID = '123'";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // Xử lý kết quả trả về
+            if (!resultSet.next()) {
+                System.out.println("Long");
+            }
+            while (resultSet.next()) {
+                String id = resultSet.getString("UserID");
+                String name = resultSet.getString("Name");
+                System.out.println(id + " and  " + name);
+                
+                // ...
+            }
+
+            // Đóng tài nguyên
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
